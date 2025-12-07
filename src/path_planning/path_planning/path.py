585fbf2e13aa45c4b8d499ae05ad43e9461c14d5
NishantM747 <nishantmiglani123@gmail.com>
2025-12-07 12:57:13 +0530

    adding comments

--- src/path_planning/path_planning/path.py
+++ src/path_planning/path_planning/path.py
@@ -6,8 +6,8 @@ from std_msgs.msg import Int16
 from mapping.mapping_shi import GeneratedMap
 from messages.srv import Mapinfo
 from messages.msg import Map
-class pathplanning(Node):
-    def __init__(self,map):
+class pathplanning(Node): 
+    def __init__(self,map): #map is a dictionary with key (x,y) and value status, with 0 for clear 
         self.map = map
         super().__init__("path_planning_node")
     
@@ -19,42 +19,8 @@ class pathplanning(Node):
                 if self.map.get(new_x,new_y) == 0:
                     neighbors[(new_x,new_y)] = self.map.get(new_x,new_y)
             return neighbors
-    def nav(self,coord1:tuple,coord2:tuple):
+    def nav(self,coord1:tuple,coord2:tuple): ## elementary path finding algorithim for now please keep the input and output types same. 
         self.get_logger().info(f"Navigating from {coord1} to {coord2}")
-        if coord2 == None:
-            return []
-        paths = [[coord1]]
-        found = False
-        path = []
-        new_paths = []
-        visited = [coord1]
-        while (not found) and paths:
-            for p in paths:
-                neigh = self.get_neighbors(p[-1])
-                for n,v in neigh.items():
-                    if v != 0:
-                        continue 
-                    repeat = False
-                    for x in visited:
-                        if x[0] == n[0] and x[1] == n[1]:
-                            repeat = True
-                    if repeat:
-                        continue
-                    new_path = p + [n]
-                    visited.append(n)
-                    new_paths.append(new_path)
-                    if n[0] == coord2[0] and n[1] == coord2[1]:
-                        found = True
-                        path = new_path
-                        break
-                if found:
-                    break
-            if found:
-                break
-            paths = new_paths
-            new_paths = []
-        return path
-    def van(self,coord1:tuple,coord2:tuple):
         if coord2 == None:
             return []
         paths = [[coord1]]
