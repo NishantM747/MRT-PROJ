c320bf39523a9eda1d3789dc9a67412a33fae550
sohanchelekar276 <sohan.chelekar@gmail.com>
2025-12-19 14:51:08 +0530

    Update sim_dashboard.py

--- src/swarm_logic/swarm_logic/sim_dashboard.py
+++ src/swarm_logic/swarm_logic/sim_dashboard.py
@@ -92,8 +92,13 @@ class DashboardNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = DashboardNode()
-    node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
